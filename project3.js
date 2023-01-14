9b8544a11067fdb2503703bfa39dcfa6886e1a06
vanumdaniel7 <vanumdaniel7@gmail.com>
2023-01-14 20:23:28 +0530

    Final Application

---
+++ project3.js
@@ -0,0 +1,18 @@
+function myfunction1()
+{
+    window.location.href='chat.html';
+}
+function myfunction2()
+{
+    window.location.href='project3.html';
+}
+function myfunction3()
+{
+    const modal=document.getElementById("modalcontainer");
+    modal.style.display="block";
+}
+function myfunction4()
+{
+    const modal=document.getElementById("modalcontainer");
+    modal.style.display="none";
+}
